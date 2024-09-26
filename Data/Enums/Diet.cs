dfee384e7c38918174ba21d837b80fe45bfa5722
Pedro Dias <146863707+Pedro-RDias@users.noreply.github.com>
2024-09-26 13:38:12 +0100

    Add enums

--- Data/Enums/Diet.cs
+++ Data/Enums/Diet.cs
@@ -1,6 +1,18 @@
-﻿namespace Blog.Data.Enums;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Diet
+namespace Blog.Data.Enums;
+
+public enum Diet
 {
+    [Display(Name = "Vegan")]
+    Vegan,
     
+    [Display(Name = "Vegetarian")]
+    Vegetarian,
+    
+    [Display(Name = "Pescatarian")]
+    Pescatarian,
+    
+    [Display(Name = "Omnivore")]
+    Omnivore
 }
