2e3630c447a287c9e170ba2cbc9695f23dd80734
Pedro Dias <146863707+Pedro-RDias@users.noreply.github.com>
2024-09-27 14:05:57 +0100

    Models (#3)

---
+++ Blog/Models/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Models;
+
+[PrimaryKey("RecipeId", "UserId")]
+public class Rating
+{
+    [Required, Range(1, 5)] public int RatingValue { get; set; }
+    
+    // Foreign keys
+    [Required, ForeignKey("Recipe")] public int RecipeId { get; set; }
+    [Required, ForeignKey("User")] public string UserId { get; set; }
+    
+    // Timestamps
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public DateTime DateUpdated { get; set; } = DateTime.Now;
+
+    // Navigation properties
+    public virtual User User { get; set; }
+    public virtual Recipe Recipe { get; set; }
+}
---
+++ Blog/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Models;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    
+    // Navigation properties
+    public virtual ICollection<Recipe> Recipes { get; set; }
+    public virtual ICollection<Comment> Comments { get; set; }
+    public virtual ICollection<Rating> Ratings { get; set; } 
+}
