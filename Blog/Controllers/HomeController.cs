affc92cedd1e8ba7a76d31d32ab4409650355231
Pedro Dias <PedroRibeiroDias@outlook.pt>
2024-10-25 22:41:43 +0100

    Add recipes to homepage

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -1,21 +1,56 @@
 using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Blog.Data;
+using Blog.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var recipes = await _context.Recipes
+                .Include(r => r.Author)
+                .Include(r => r.Comments)
+                .Include(r => r.Ratings)
+                .Include(r => r.Ingredients)
+                .Include(r => r.PreparationSteps)
+                .OrderByDescending(r => r.DateCreated)
+                .ToListAsync();
+            
+            var dates = new Dictionary<int, List<int>>();
+            foreach (var recipe in recipes)
+            {
+                if (!dates.ContainsKey(recipe.DateCreated.Year))
+                {
+                    dates[recipe.DateCreated.Year] = new List<int>();
+                }
+
+                if (!dates[recipe.DateCreated.Year].Contains(recipe.DateCreated.Month))
+                {
+                    dates[recipe.DateCreated.Year].Add(recipe.DateCreated.Month);
+                }
+            }
+            
+            var viewModel = new RecipesViewIndexModel()
+            {
+                Recipes = recipes,
+                Dates = dates
+            };
+            
+            
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
---
+++ Blog/ViewModels/RecipesViewIndexModel.cs
@@ -0,0 +1,9 @@
+﻿using Blog.Models;
+
+namespace Blog.ViewModels;
+
+public class RecipesViewIndexModel
+{
+    public List<Recipe> Recipes { get; set; }
+    public Dictionary<int, List<int>> Dates { get; set; }
+}
