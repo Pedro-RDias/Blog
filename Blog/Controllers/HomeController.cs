c9cc6eabce9ab9e8ab2431f2eb9e0345db5eccde
João Monteiro <monteiro_1998@hotmail.com>
2024-11-08 17:12:01 +0000

    Algum Design

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -129,4 +129,14 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
 }
